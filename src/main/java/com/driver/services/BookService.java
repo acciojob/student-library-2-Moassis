576d7f4e0bcc69c8e4da32bd4f5015150d3f82ae
Moassis <moassis.rommel@gmail.com>
2023-01-18 11:58:15 +0530

    5

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -6,7 +6,6 @@ import com.driver.repositories.AuthorRepository;
 import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -32,19 +31,15 @@ public class BookService {
     }
 
     public List<Book> getBooks(String genre, boolean available, String author) {
-        List<Book> books = new ArrayList<>(); // find the elements of the list by yourself
-        if (genre == null && author != null)
-            books = bookRepository2.findBooksByAuthor(author, available);
+        if (genre != null && available == true && author != null)
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
 
-        else if (genre != null && author == null && available) {
-            books = bookRepository2.findBooksByGenre(genre, available);
-        } else if (genre != null && author != null && !available) {
-            books = bookRepository2.findBooksByGenreAuthor(genre, author, available);
-        } else if (genre != null && author != null && available) {
-            books = bookRepository2.findBooksByGenreAuthor(genre, author, available);
-        } else if (genre == null && author == null && available)
-            books = bookRepository2.findByAvailability(available);
+        else if (author != null)
+            return bookRepository2.findBooksByAuthor(author, available);
+        else if (genre != null)
+            return bookRepository2.findBooksByGenre(genre, available);
+        else
+            return bookRepository2.findByAvailability(available);
 
-        return books;
     }
 }
