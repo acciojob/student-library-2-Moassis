07780ec112fa0cf76f70676579ac548dd95cc02a
Moassis <moassis.rommel@gmail.com>
2023-01-27 15:12:08 +0530

    3

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -6,7 +6,6 @@ import com.driver.repositories.AuthorRepository;
 import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,23 +22,32 @@ public class BookService {
 
         // for Book Repository
         book.setAvailable(true);
+        Author author = book.getAuthor();
+        if (author == null) {
+            bookRepository2.save(book);
+        }
 
         // for author repository
-        Author author = book.getAuthor();
-        List<Book> books = author.getBooksWritten();
-        if (books == null)
-            books = new ArrayList<>();
-        books.add(book);
-        author.setBooksWritten(books);
-        authorRepository.save(author);
+        else {
+            List<Book> books = author.getBooksWritten();
+            if (books == null)
+                books = new ArrayList<>();
+            books.add(book);
+            author.setBooksWritten(books);
+            authorRepository.save(author);
+        }
+
     }
 
     public List<Book> getBooks(String genre, boolean available, String author) {
-        List<Book> books = null;
-        if (genre != null && available == true && author == null)
-            books = bookRepository2.findBooksByGenre(genre, available);
-        else if (genre != null && available == false && author != null)
-            books = bookRepository2.findBooksByGenreAuthor(genre, author, available);
-        return books;
+        if (genre != null && author != null) {
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        } else if (genre != null) {
+            return bookRepository2.findBooksByGenre(genre, available);
+        } else if (author != null) {
+            return bookRepository2.findBooksByAuthor(author, available);
+        } else {
+            return bookRepository2.findByAvailability(available);
+        }
     }
 }
